8079ed6605cc7678baa1c362ce03b3ae581281bc
sohyun329 <58158103+sohyun329@users.noreply.github.com>
2024-07-30 13:55:04 +0900

    week04_day02_Binary Search

---
+++ 나무자르기_2805.py
@@ -0,0 +1,25 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().strip().split())
+trees = sorted(list(map(int, sys.stdin.readline().split())))
+
+def solve(m,trees):
+    answer = 0
+    start, end = 0, max(trees)
+    
+    while start <= end:
+        mid = (start+end) // 2
+        length = 0
+        for t in trees:
+            if t > mid:
+                length += t - mid
+        
+        if length >= m :
+            answer = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+            
+    return answer
+
+print(solve(m, trees))
